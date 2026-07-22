445d2cd24f514557b7d8b4cec953958d791c9d22
serialv2 <154690894+serialv2@users.noreply.github.com>
2026-07-22 20:50:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Ne placez jamais la clé "service_role" dans un site web.
 
 export const SUPABASE_URL = "https://keeraqtoiwvcybhavkfb.supabase.co";
-export const SUPABASE_PUBLIC_KEY = "COLLEZ_ICI_VOTRE_CLE_PUBLIQUE_SUPABASE";
+export const SUPABASE_PUBLIC_KEY = "sb_publishable_aFPzJE3WbzLhJqJjgE92ng_HQbOTfPf";
